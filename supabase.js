5250379cf88fe7fe4dd10ca6a01568dcbf38342a
amii1005 <mmyparoe@gmail.com>
2026-09-11 15:37:52 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -4,7 +4,7 @@ const SUPABASE_URL =
 const SUPABASE_KEY =
     "sb_publishable_kLdCZ4D1xPR5jpTpENFxSA_Y9iuGtjz";
 
-const supabaseClient =
+const supabase =
     window.supabase.createClient(
         SUPABASE_URL,
         SUPABASE_KEY
