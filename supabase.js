4d844afb136f115a10eb7b8da8eea962baff7308
amii1005 <mmyparoe@gmail.com>
2026-09-11 13:09:46 +0800

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,11 @@
+const SUPABASE_URL =
+    "https://sycuueukzazluxoyhozn.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_kLdCZ4D1xPR5jpTpENFxSA_Y9iuGtjz";
+
+const supabaseClient =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
